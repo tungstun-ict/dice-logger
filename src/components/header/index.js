a7bf9338ff94feb9767f0df162262dc7f52644d3
Jort Willemsen <jort@maurice-nienke.nl>
2022-03-19 00:44:38 +0100

    Add header component skeleton

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header__background">
+            <p>I am a header</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
