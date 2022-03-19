a50ceacab5eec62e984803ff9fc6e30afd93452b
Jort Willemsen <jort@maurice-nienke.nl>
2022-03-19 01:51:29 +0100

    Add simple header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,20 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="header__background">
-            <h1 className="header__title">dice logger</h1>
+          <img
+            className="header__logo"
+            src={require("../../assets/images/logo.png")}
+            alt="logo"
+          />
+          <h1 className="header__title">dice logger</h1>
+
+          <div className="header__tools">
+          <img
+            className="header__profile"
+            src={require("../../assets/images/profile-picture.png")}
+            alt="profile"
+          />
+          </div>
         </div>
       </header>
     );
