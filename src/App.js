e68cd5fb1587762b0a85a8b91bf82b250a326d4a
Jort Willemsen <jort@maurice-nienke.nl>
2022-03-21 15:28:43 +0100

    Design window component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Header from './components/header';
-import Window from './components/window';
+import "./App.css";
+import Header from "./components/header";
+import Window from "./components/window";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="window_bounds">
-        <Window name="Window 1"/>
-        <Window name="Window 2"/>
+        <Window name="Window 2" />
+        <Window name="Window 1" />
       </div>
     </div>
   );
--- src/components/window/index.js
+++ src/components/window/index.js
@@ -1,15 +1,24 @@
+import { useRef, useState } from "react";
 import Draggable from "react-draggable";
 import "./window.css"
 
 function Window(props) {
+  const nodeRef = useRef(null);
+  const [selected, setSelected] = useState(0);
+  
+  const handleClick = () => {
+    console.log(`selected: ` + props.name)
+    setSelected(selected + 1)
+  }
+  
   return (
-    <Draggable bounds="parent" handle=".window__header">
-      <div className="window">
+    <Draggable nodeRef={nodeRef} bounds="parent" handle=".window__header">
+      <div ref={nodeRef} className="window" style={{zIndex: selected}} onClick={handleClick}>
         <div className="window__header">
           <h2 className="window__title">{props.name}</h2>
           <div className="window__close"></div>
         </div>
-        <p>Content</p>
+        <p>{selected}</p>
       </div>
     </Draggable>
   );
